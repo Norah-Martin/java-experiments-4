2dccad5857faff61cabdbb002e54e53f4cae9484
Norah Martin <93730785+Norah-Martin@users.noreply.github.com>
2022-02-23 08:49:03 +0530

    Update Employee_Engineer.java

--- Employee_Engineer.java
+++ Employee_Engineer.java
@@ -1,35 +1,79 @@
 import java.util.*;
-class Employee
+class employee
 {
-    void display()
-    {
-        System.out.println("Name of the class is Employee");
-    }
-
-    int calcSalary()
-    {
-        System.out.println("Salary of employee is 10000");
-        return 0;
-    }
+ Scanner obj = new Scanner(System.in);
+ String name;
+ int age;
+ long phn;//phone number
+ String ads;//address
+ float sly;//salary
+ void printSalary()
+ {
+  System.out.println("Salary:"+sly);
+ }
+ void read()
+ {
+  System.out.println("Enter name,address,age,phone_number,salary");
+  this.name = obj.nextLine();
+  this.ads = obj.nextLine();
+  this.age = obj.nextInt();
+  this.phn = obj.nextLong();
+  this.sly = obj.nextFloat();
+ }
+ void print()
+ {
+  System.out.println("Name:"+this.name);
+  System.out.println("Age:"+this.age);
+  System.out.println("Ph.No.:"+this.phn);
+  System.out.println("Address:"+this.ads);
+  this.printSalary();
+ }
 }
-
-class Engineer extends Employee
+class officer extends employee
 {
-   
-    int calcSalary()
-    {
-        super.calcSalary();
-    System.out.println("Salary of employee is 20000");
-    return 0;
-    }
+ Scanner obj = new Scanner(System.in);
+ String spn;//specialization
+ void read()
+ {
+  super.read();
+  System.out.println("Specialization:");
+  this.spn = obj.nextLine();
+ }
+ void print()
+ {
+  super.print();
+  System.out.println("Specialization:"+this.spn);
+ }
 }
-
-class Testclass
+class manager extends employee
+{ 
+ Scanner obj = new Scanner(System.in);
+ String dep;//department
+ void read()
+ {
+  super.read();
+  System.out.println("Department:");
+  this.dep = obj.nextLine();
+ }
+ void print()
+ { 
+  super.print();
+  System.out.println("Department:"+this.dep);
+ }
+}
+class office
 {
-    public static void main(String args [])
-    {
-        Engineer s=new Engineer();
-        s.display();
-        s.calcSalary();
-     }
+ public static void main(String args[])
+ {
+  officer o = new officer();
+  manager m = new manager();
+  o.read();
+  System.out.println("####################");
+  m.read();
+  System.out.println("\n\n");
+  o.print();
+  System.out.println("####################");
+  m.print();
+ }
+ 
 }
